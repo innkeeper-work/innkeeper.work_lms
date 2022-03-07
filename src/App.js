a23609e12d5b0a9aa2950e3c2bfccab68e635a64
profgreatwonder <muoghaluchidinma@gmail.com>
2022-03-07 09:28:42 +0100

    folder structure updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle";
+import "./sassfiles/app.css";
+// revisit, buttons have not achieved gradient in design
+// import { SignInButton } from "./components/Buttons";
+// import { GetStartedButton } from "./components/Buttons";
+// import { LearnMoreButton } from "./components/Buttons";
+// import { BeginCoursesButton } from "./components/Buttons";
+// import { ForwardButton } from "./components/Buttons";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return <div className="App"></div>;
 }
 
 export default App;
---
+++ src/components/Buttons.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+export const SignInButton = () => {
+	return (
+		<button type="button" className="btn btn-info signinbtn">
+			Sign In
+		</button>
+	);
+};
+export const GetStartedButton = () => {
+	return (
+		<button type="button" className="btn btn-info callToAction">
+			Get Started
+		</button>
+	);
+};
+export const LearnMoreButton = () => {
+	return (
+		<button type="button" className="btn btn-info callToAction">
+			Learn More
+		</button>
+	);
+};
+export const BeginCoursesButton = () => {
+	return (
+		<button type="button" className="btn btn-info callToAction">
+			Begin Courses
+		</button>
+	);
+};
+export const ForwardButton = () => {
+	return (
+		<button type="button" className="btn btn-info callToAction">
+			Forward
+		</button>
+	);
+};
