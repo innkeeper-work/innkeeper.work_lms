045a2a318244de94fff05fff270ba001ea521684
Adebanjo Ebenezer Adeniji <aeben.adebanjo@gmail.com>
2022-03-09 09:56:57 +0100

    first commit

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,7 +2,9 @@ import React from "react";
 import barter from "../images/barter.jpg";
 
 export const HomepageHero = () => {
-	return <div></div>;
+	return <div>
+	<img src={barter} alt=''/>
+	</div>;
 };
 export const CoursepageHero = () => {
 	return <div></div>;
