b7fa9100c5113f1474704cfa4cbb711e640e7f0c
Raphael Ovonlen <princely233@gmail.com>
2022-03-15 19:13:02 +0100

    Footer responsiveness completed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,18 +8,15 @@ import LinkedInIcon from '@material-ui/icons/LinkedIn';
 const Footer = () => {
   return (
     <>
-      <footer className="text-center text-lg-start bgSoft">
+      <footer className="text-left text-lg-start bgSoft">
         <div className="card-body herotextWhite">
-          <div className="container text-center text-lg-start mt-5">
-            <div className="row mt-3">
-              <div
-                className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4"
-                style={{ textAlign: 'left' }}
-              >
+          <div className="container text-left text-lg-start mt-5">
+            <div className="row mt-3 ms-5">
+              <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
                 <img src={innkeeperlogo} alt="Innkeeper.work" />
-                <h6 className="text-uppercase fw-bold mb-4">
+                <h5 className="text-uppercase fw-bold mb-4">
                   <span />
-                </h6>
+                </h5>
                 <p>
                   The 8thGear Hub, 11b Colin Onabule Crescent, Diamond Estate
                   Off CMD Road (beside Secretariat/Magodo gate) Lagos Nigeria.
@@ -27,8 +24,8 @@ const Footer = () => {
                 </p>
               </div>
 
-              <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4 d-none d-md-block">
-                <h6 className="text-uppercase fw-bold mb-4">Menu</h6>
+              <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4 d-none d-md-block ms-5">
+                <h5 className="text-uppercase fw-bold mb-4">Menu</h5>
                 <p>
                   <a
                     href="#!"
@@ -76,8 +73,8 @@ const Footer = () => {
                 </p>
               </div>
 
-              <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4 d-none d-md-block">
-                <h6 className="text-uppercase fw-bold mb-4">Courses</h6>
+              <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4 d-none d-md-block">
+                <h5 className="text-uppercase fw-bold mb-4">Courses</h5>
                 <p>
                   <a
                     href="#!"
@@ -116,70 +113,72 @@ const Footer = () => {
                 </p>
               </div>
 
-              <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4 d-flex d-md-block">
-                <p>
-                  <a
-                    className="btnText d-flex"
-                    href="https://facebook.com/"
-                    style={{ textDecoration: 'none', marginRight: '50px' }}
-                  >
-                    <FacebookIcon />
-                    <p
-                      className="d-none d-md-block "
-                      style={{ textDecoration: 'none', marginLeft: '10px' }}
+              <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                <div className="d-flex d-md-block ms-5">
+                  <p>
+                    <a
+                      className="btnText d-flex"
+                      href="https://facebook.com/"
+                      style={{ textDecoration: 'none', marginRight: '50px' }}
                     >
-                      Facebook
-                    </p>
-                  </a>
-                </p>
-                <p>
-                  <a
-                    className="btnText d-flex"
-                    href="https://twitter.com/"
-                    style={{ textDecoration: 'none', marginRight: '50px' }}
-                  >
-                    <TwitterIcon />
-                    <p
-                      className="d-none d-md-block "
-                      style={{ textDecoration: 'none', marginLeft: '10px' }}
+                      <FacebookIcon />
+                      <p
+                        className="d-none d-md-block "
+                        style={{ textDecoration: 'none', marginLeft: '5px' }}
+                      >
+                        Facebook
+                      </p>
+                    </a>
+                  </p>
+                  <p>
+                    <a
+                      className="btnText d-flex"
+                      href="https://twitter.com/"
+                      style={{ textDecoration: 'none', marginRight: '50px' }}
                     >
-                      {' '}
-                      Twitter
-                    </p>
-                  </a>
-                </p>
-                <p>
-                  <a
-                    className="btnText d-flex"
-                    href="https://instagram.com/"
-                    style={{ textDecoration: 'none', marginRight: '50px' }}
-                  >
-                    <InstagramIcon />
-                    <p
-                      className="d-none d-md-block "
-                      style={{ textDecoration: 'none', marginLeft: '10px' }}
+                      <TwitterIcon />
+                      <p
+                        className="d-none d-md-block "
+                        style={{ textDecoration: 'none', marginLeft: '5px' }}
+                      >
+                        {' '}
+                        Twitter
+                      </p>
+                    </a>
+                  </p>
+                  <p>
+                    <a
+                      className="btnText d-flex"
+                      href="https://instagram.com/"
+                      style={{ textDecoration: 'none', marginRight: '50px' }}
                     >
-                      {' '}
-                      Instagram
-                    </p>
-                  </a>
-                </p>
-                <p>
-                  <a
-                    className="btnText d-flex"
-                    href="https://linked.com/"
-                    style={{ textDecoration: 'none', marginRight: '50px' }}
-                  >
-                    <LinkedInIcon />
-                    <p
-                      className="d-none d-md-block "
-                      style={{ textDecoration: 'none', marginLeft: '10px' }}
+                      <InstagramIcon />
+                      <p
+                        className="d-none d-md-block "
+                        style={{ textDecoration: 'none', marginLeft: '5px' }}
+                      >
+                        {' '}
+                        Instagram
+                      </p>
+                    </a>
+                  </p>
+                  <p>
+                    <a
+                      className="btnText d-flex"
+                      href="https://linked.com/"
+                      style={{ textDecoration: 'none', marginRight: '50px' }}
                     >
-                      {' '}
-                      Linkedin
-                    </p>
-                  </a>
-                </p>
+                      <LinkedInIcon />
+                      <p
+                        className="d-none d-md-block "
+                        style={{ textDecoration: 'none', marginLeft: '5px' }}
+                      >
+                        {' '}
+                        Linkedin
+                      </p>
+                    </a>
+                  </p>
+                </div>
               </div>
             </div>
           </div>
