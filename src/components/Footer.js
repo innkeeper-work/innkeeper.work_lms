7da4d8ee2f10549db9eaa544f30addb7c5e6ef1c
profgreatwonder <muoghaluchidinma@gmail.com>
2022-03-21 14:44:22 +0100

    conflict resolution with error404

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,138 +2,102 @@ import React from "react";
 import innkeeperlogo from "../images/innkeeperlogo.png";
 import {
 	FacebookIcon,
+	WhatsAppIcon,
 	InstagramIcon,
 	LinkedInIcon,
-	WhatsAppIcon,
 } from "../components/Icons";
 import { Link } from "react-router-dom";
 
 const Footer = () => {
 	return (
 		<>
-			<footer className="text-left text-lg-start bgSoft">
-				<div className="card-body herotextWhite">
-					<div className="container text-left text-lg-start mt-5">
-						<div className="row mt-3 ms-5">
-							<div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-								<img src={innkeeperlogo} alt="Innkeeper.work" />
-								<h5 className="text-uppercase fw-bold mb-4">
-									<span />
-								</h5>
-								<p>
-									The 8thGear Hub, 11b Colin Onabule Crescent, Diamond Estate
-									Off CMD Road (beside Secretariat/Magodo gate) Lagos Nigeria.
-									+234 (0) 809-846-8885 info@innkeeper.work
-								</p>
-							</div>
+			<footer className=" bgSoft ">
+				<div className="container-fluid  justify-content-evenly p-4">
+					<div className="row herotextWhite d-flex justify-content-evenly mt-5">
+						<div className="col-md-3 col-lg-4 col-xl-3 mx-auto">
+							<img src={innkeeperlogo} alt="Innkeeper.work" />
 
-							<div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4 d-none d-md-block ms-5">
-								<h5 className="text-uppercase fw-bold mb-4">Menu</h5>
-								<p>
-									<Link
-										to="businessupport"
-										className="text-reset"
-										style={{ textDecoration: "none" }}>
-										Business Support Academy
-									</Link>
-								</p>
+							<p>
+								The 8thGear Hub, 11b Colin Onabule Crescent, Diamond Estate Off
+								CMD Road (beside Secretariat/Magodo gate) Lagos Nigeria. +234
+								(0) 809-846-8885 info@innkeeper.work
+							</p>
+						</div>
+
+						<div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4 d-none d-md-block ms-3 ps-5 gs-5">
+							<h5 className="text-uppercase fw-bold mb-4">Menu</h5>
+							<p>Home</p>
+							<p>About Us</p>
+							<p>Events</p>
+							<p>Contact</p>
+							<p>FAQ</p>
+						</div>
+
+						<div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4 d-none d-md-block ms-5 gs-5">
+							<h5 className="text-uppercase fw-bold mb-4">Courses</h5>
+							<p>Business Support Academy</p>
+							<p>Tech Academy</p>
+							<p>Employability / Soft&nbsp;Skills</p>
+							<p>Entrepreneurship</p>
+						</div>
+
+						<div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4 d-none d-md-block ms-5">
+							<h5 className="text-uppercase fw-bold mb-4">Menu</h5>
+							<p>
 								<Link
-									to="techacademy"
+									to="/"
 									className="text-reset"
 									style={{ textDecoration: "none" }}>
-									Tech Academy
+									Home
 								</Link>
-								{/* </p> */}
-								<p>
-									<Link
-										to="employability"
-										className="text-reset"
-										style={{ textDecoration: "none" }}>
-										Employability/Soft Skills
-									</Link>
-								</p>
-								<p>
-									<Link
-										to="entrepreneurship"
-										className="text-reset"
-										style={{ textDecoration: "none" }}>
-										Entrepreneurship
-									</Link>
-								</p>
-							</div>
+							</p>
+							<p>
+								<a
+									href="#!"
+									className="text-reset"
+									style={{ textDecoration: "none" }}>
+									Events
+								</a>
+							</p>
+							<p>
+								<Link
+									to="contact"
+									className="text-reset"
+									style={{ textDecoration: "none" }}>
+									Contact
+								</Link>
+							</p>
+							<a
+								href="#faq"
+								className="text-reset"
+								style={{ textDecoration: "none" }}>
+								FAQ
+							</a>
+						</div>
 
-							<div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4 sm-3">
-								<div className="d-flex d-md-block ms-5 flex-wrap">
-									<p>
-										<a
-											href="https://www.facebook.com/innkeeper.work/"
-											className="btnText d-flex"
-											target="_blank"
-											rel="noreferrer"
-											style={{ textDecoration: "none", marginRight: "50px" }}>
-											<FacebookIcon />
-											<p
-												className="d-none d-md-block "
-												style={{ textDecoration: "none", marginLeft: "5px" }}>
-												Facebook
-											</p>
-										</a>
-									</p>
-									<p>
-										<a
-											href="https://twitter.com/"
-											className="btnText d-flex"
-											target="_blank"
-											rel="noreferrer"
-											style={{ textDecoration: "none", marginRight: "50px" }}>
-											<WhatsAppIcon />
-											<p
-												className="d-none d-md-block "
-												style={{ textDecoration: "none", marginLeft: "5px" }}>
-												{" "}
-												WhatsApp
-											</p>
-										</a>
-									</p>
-									<p>
-										<a
-											href="https://www.instagram.com/innkeeper.work__/"
-											className="btnText d-flex"
-											target="_blank"
-											rel="noreferrer"
-											style={{ textDecoration: "none", marginRight: "50px" }}>
-											<InstagramIcon />
-											<p
-												className="d-none d-md-block "
-												style={{ textDecoration: "none", marginLeft: "5px" }}>
-												{" "}
-												Instagram
-											</p>
-										</a>
-									</p>
-									<p>
-										<a
-											href="https://www.linkedin.com/company/innkeeper-work/"
-											className="btnText d-flex"
-											target="_blank"
-											rel="noreferrer"
-											style={{ textDecoration: "none", marginRight: "50px" }}>
-											<LinkedInIcon />
-											<p
-												className="d-none d-md-block "
-												style={{ textDecoration: "none", marginLeft: "5px" }}>
-												{" "}
-												Linkedin
-											</p>
-										</a>
-									</p>
+						<div className="col-md-2 col-lg-2 col-xl-2 mx-auto gs-5 mx-auto mb-md-0 mb-4 sm-3">
+							<div className="d-flex justify-content-evenly d-md-block">
+								<div className="btnText d-flex justify-content-end px-4 pb-3">
+									<FacebookIcon />
+									<p className="d-none d-md-block ms-3">Facebook</p>
+								</div>
+								<div className="btnText d-flex justify-content-end px-4 pb-3">
+									<WhatsAppIcon />
+									<p className="d-none d-md-block ms-3">WhatsApp</p>
+								</div>
+								<div className="btnText d-flex justify-content-end px-4 pb-3">
+									<InstagramIcon />
+									<p className="d-none d-md-block ms-3">Instagram</p>
+								</div>
+								<div className="btnText d-flex justify-content-end px-4 pb-3">
+									<LinkedInIcon />
+									<p className="d-none d-md-block ms-3 me-3">Linkedin</p>
 								</div>
 							</div>
 						</div>
 					</div>
 				</div>
 			</footer>
-			//{" "}
 		</>
 	);
 };
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -16,10 +16,10 @@ export let HomepageHero = () => {
 						loading=""
 					/>
 				</div>
-				<div className="col-lg-7 ps-lg-5 ps-md-5 col-md-6 py-5">
+				<div className="col-lg-7 ps-5 col-md-6 py-5">
 					<h1 className="herotextWhite">
 						Upskill for this&nbsp;
-						<span className="herotextColored d-flex">Digital age.</span>
+						<span className="herotextColored">Digital age.</span>
 					</h1>
 					<p className="herotextWhite">
 						Mentored Upskilling For Improved Learning Opportunities. Get the
@@ -37,15 +37,14 @@ export let HomepageHero = () => {
 export const CoursepageHero = (props) => {
 	const { herotitle, Description } = props;
 	return (
-		<div className="py-2 heroBg">
+		<div className="px-4 py-2 heroBg">
 			<Navbar />
-
-			<div className="row g-0 p-5">
+			<div className="row align-items-center g-5 p-md-4 m-5">
 				<div className="col-lg-6 col-sm-12">
 					<h1 className="herotextColored">
 						{herotitle} <br />
+						<p className="herotextWhite">{Description}</p>
 					</h1>
-					<p className="herotextWhite">{Description}</p>
 					<GetStartedButton />
 				</div>
 			</div>
