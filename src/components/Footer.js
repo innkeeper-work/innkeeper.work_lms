490d65a37ad44fc5952b71e028060bbf17a303b8
profgreatwonder <muoghaluchidinma@gmail.com>
2022-03-26 00:16:15 +0100

    switched from hashrouter to browserrouter

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,5 @@
 import React from "react";
 import innkeeperlogo from "../images/innkeeperlogo.png";
-
 import {
 	FacebookIcon,
 	WhatsAppIcon,
@@ -29,7 +28,7 @@ const Footer = () => {
 										The 8thGear Hub, 11b Colin Onabule Crescent, Diamond Estate
 										Off CMD Road (beside Secretariat/Magodo gate) Lagos Nigeria.
 									</a>
-									<div className="mt-2 pt-2">
+									<p className="mt-2 pt-2">
 										+234 (0) 809-846-8885
 										<p>
 											<a
@@ -41,132 +40,117 @@ const Footer = () => {
 												info@innkeeper.work
 											</a>
 										</p>
-									</div>
+									</p>
 								</div>
 							</div>
 
-							<div className="mt-2 pt-2">
-								<a
-									href=" https://goo.gl/maps/6TWAwZbE7ab3rLoz9"
-									className="mt-4 pt-2 text-reset"
-									target="_blank"
-									rel="noreferrer"
-									style={{ textDecoration: "none" }}>
-									The 8thGear Hub, 11b Colin Onabule Crescent, Diamond Estate
-									Off CMD Road (beside Secretariat/Magodo gate) Lagos Nigeria.
-								</a>
-								<p className="mt-2 pt-2">
-									+234 (0) 809-846-8885
-									<p>
-										<a
-											href="mailto:info@innkeeper.work"
-											className="text-reset"
-											target="_blank"
-											rel="noreferrer"
-											style={{ textDecoration: "none" }}>
-											info@innkeeper.work
-										</a>
-									</p>
+							<div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4 d-none d-md-block ms-5 ">
+								<h5 className="text-uppercase fw-bold mb-4">Menu</h5>
+								<p>
+									<Link
+										to="/"
+										className="text-reset"
+										style={{ textDecoration: "none" }}>
+										Home
+									</Link>
 								</p>
+								<p>
+									<a
+										href="#!"
+										className="text-reset"
+										style={{ textDecoration: "none" }}>
+										Events
+									</a>
+								</p>
+								<p>
+									<Link
+										to="contact"
+										className="text-reset"
+										style={{ textDecoration: "none" }}>
+										Contact
+									</Link>
+								</p>
+								<a
+									href="#faq"
+									className="text-reset"
+									style={{ textDecoration: "none" }}>
+									FAQ
+								</a>
 							</div>
-						</div>
 
-						<div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4 d-none d-md-block ms-5 ">
-							<h5 className="text-uppercase fw-bold mb-4">Menu</h5>
-							<p>
+							<div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4 d-none d-md-block me-5">
+								<h5 className="text-uppercase fw-bold mb-4">Courses</h5>
 								<Link
-									to="/"
-									className="text-reset"
+									to="businesssupportacademy"
+									className="text-reset btnText d-flex justify-content-start  pb-3"
 									style={{ textDecoration: "none" }}>
-									Home
+									Business Support Academy
 								</Link>
-							</p>
-							<p>
-								<a
-									href="#!"
-									className="text-reset"
-									style={{ textDecoration: "none" }}>
-									Events
-								</a>
-							</p>
-							<p>
+
 								<Link
-									to="contact"
-									className="text-reset"
+									to="techacademy"
+									className="text-reset btnText d-flex justify-content-start  pb-3"
 									style={{ textDecoration: "none" }}>
-									Contact
+									Tech Academy
 								</Link>
-							</p>
-							<a
-								href="#faq"
-								className="text-reset"
-								style={{ textDecoration: "none" }}>
-								FAQ
-							</a>
-						</div>
 
-						<div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4 d-none d-md-block me-5">
-							<h5 className="text-uppercase fw-bold mb-4">Courses</h5>
-							<Link
-								to="businesssupportacademy"
-								className="text-reset btnText d-flex justify-content-start  pb-3"
-								style={{ textDecoration: "none" }}>
-								Business Support Academy
-							</Link>
-
-							<Link
-								to="techacademy"
-								className="text-reset btnText d-flex justify-content-start  pb-3"
-								style={{ textDecoration: "none" }}>
-								Tech Academy
-							</Link>
-
-							<Link
-								to="employability"
-								className="text-reset btnText d-flex justify-content-start  pb-3"
-								style={{ textDecoration: "none" }}>
-								Employability / Soft&nbsp;Skills
-							</Link>
-
-							<Link
-								to="entrepreneurship"
-								className="text-reset btnText d-flex justify-content-start  pb-3"
-								style={{ textDecoration: "none" }}>
-								Entrepreneurship
-							</Link>
-						</div>
-
-						<div className=" col-md-2 col-lg-2 col-xl-2 mx-auto gs-5 mx-auto mb-md-0 mb-4 sm-3">
-							<div className="d-flex justify-content-evenly d-md-block">
-								<a
-									href="https://www.facebook.com/innkeeper.work/"
-									className="btnText d-flex justify-content-end px-3 pb-3 text-reset"
-									target="_blank"
-									rel="noreferrer"
+								<Link
+									to="employability"
+									className="text-reset btnText d-flex justify-content-start  pb-3"
 									style={{ textDecoration: "none" }}>
-									<FacebookIcon />
-									<p className="d-none d-md-block ms-3">Facebook</p>
-								</a>
+									Employability / Soft&nbsp;Skills
+								</Link>
 
-								<a
-									href="https://wa.link/nc43zv"
-									className="btnText d-flex justify-content-end px-3 pb-3 text-reset"
-									target="_blank"
-									rel="noreferrer"
+								<Link
+									to="entrepreneurship"
+									className="text-reset btnText d-flex justify-content-start  pb-3"
 									style={{ textDecoration: "none" }}>
-									<WhatsAppIcon />
-									<p className="d-none d-md-block ms-3">WhatsApp</p>
-								</a>
+									Entrepreneurship
+								</Link>
+							</div>
 
-								<a
-									href="https://www.linkedin.com/company/innkeeper-work/"
-									className="btnText d-flex justify-content-end px-3 pb-3 text-reset"
-									target="_blank"
-									rel="noreferrer"
-									style={{ textDecoration: "none" }}>
-									<LinkedInIcon />
-									<p className="d-none d-md-block ms-3 me-3">Linkedin</p>
-								</a>
+							<div className=" col-md-2 col-lg-2 col-xl-2 mx-auto gs-5 mx-auto mb-md-0 mb-4 sm-3">
+								<div className="d-flex justify-content-evenly d-md-block">
+									<a
+										href="https://www.facebook.com/innkeeper.work/"
+										className="btnText d-flex justify-content-end px-3 pb-3 text-reset"
+										target="_blank"
+										rel="noreferrer"
+										style={{ textDecoration: "none" }}>
+										<FacebookIcon />
+										<p className="d-none d-md-block ms-3">Facebook</p>
+									</a>
+
+									<a
+										href="https://wa.link/nc43zv"
+										className="btnText d-flex justify-content-end px-3 pb-3 text-reset"
+										target="_blank"
+										rel="noreferrer"
+										style={{ textDecoration: "none" }}>
+										<WhatsAppIcon />
+										<p className="d-none d-md-block ms-3">WhatsApp</p>
+									</a>
+
+									<a
+										href="https://www.instagram.com/innkeeper.work/"
+										className="btnText d-flex justify-content-end px-3 pb-3 text-reset"
+										target="_blank"
+										rel="noreferrer"
+										style={{ textDecoration: "none" }}>
+										<InstagramIcon />
+										<p className="d-none d-md-block ms-3">Instagram</p>
+									</a>
+
+									<a
+										href="https://www.linkedin.com/company/innkeeper-work/"
+										className="btnText d-flex justify-content-end px-3 pb-3 text-reset"
+										target="_blank"
+										rel="noreferrer"
+										style={{ textDecoration: "none" }}>
+										<LinkedInIcon />
+										<p className="d-none d-md-block ms-3 me-3">Linkedin</p>
+									</a>
+								</div>
 							</div>
 						</div>
 					</div>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { HashRouter as Router } from "react-router-dom";
+// import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -9,9 +10,9 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import "./sassfiles/app.css";
 
 ReactDOM.render(
-	<Router>
+	<BrowserRouter>
 		<App />
-	</Router>,
+	</BrowserRouter>,
 	document.getElementById("root")
 );
 
