3fecb5f3c310ddc9c050558a8d313631a3a83d3e
kingdom-addict <aeben.adebanjo@gmail.com>
2022-04-06 18:04:00 +0100

    changed soft.innkeeper.work to app.innkeeper.work

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export const SignInButton = () => {
   return (
-    <a href="https://soft.innkeeper.work/login/index.php" rel="noreferrer">
+    <a href="https://app.innkeeper.work/login/index.php" rel="noreferrer">
       <button type="button" className="btn signinbtnapply">
         Sign In
       </button>
