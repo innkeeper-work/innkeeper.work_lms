5c4e92d8e56c93cf07d5d176b149a802c399bab9
Opeyemi128 <opeyemibioku@gmail.com>
2022-08-03 17:13:04 +0100

    Worked on the sass folder structure

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import "./sassfiles/app.css";
+import "./sassfiles/styles.min.css";
 
 ReactDOM.render(
   <BrowserRouter>
