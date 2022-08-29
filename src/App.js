accf8ba9c93580586fc6fdc784134b3897f5cbe3
Olasunkanmiidris15@gmail.com <Olasunkanmiidris15@gmail.com>
2022-08-29 16:26:49 +0100

    build:Leaderboard Card and TasksCard

--- src/App.js
+++ src/App.js
@@ -51,67 +51,71 @@ import DigitalAdminTools from "./pages/Courseinfo/business_support/DigitalAdminT
 // import Successfulteam from "./pages/Courseinfo/archived/Successfulteam";
 // import Workshop from "./pages/Courseinfo/archived/Workshop";
 
+import { LeaderboardCard } from "./components/AllCards/InnkeeperPhaseTwoCards/AllTasksCards";
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        {/* pages */}
-        <Route path="/" element={<Homepage />} />
-        <Route path="contact" element={<Contact />} />
-        <Route path="techacademy" element={<TechAcademy />} />
-        <Route
-          path="businesssupportacademy"
-          element={<BusinessSupportAcademy />}
-        />
-        <Route path="employability" element={<Employability />} />
-        <Route path="entrepreneurship" element={<Entrepreneurship />} />
-        <Route path="studentloan" element={<Studentloan />} />
-        <Route path="scholarships" element={<Scholarships />} />
-        <Route path="trainingbond" element={<Trainingbond />} />
-        <Route path="careers" element={<Careers />} />
-        {/* <Route path="vacancy" element={<Vacancy />} /> */}
-        <Route path="privacypolicy" element={<PrivacyPolicy />} />
-        <Route path="underconstruction" element={<UnderConstruction />} />
-        <Route path="*" element={<Error404 />} />
+    // <div className="App">
+    //   <Routes>
+    //     {/* pages */}
+    //     <Route path="/" element={<Homepage />} />
+    //     <Route path="contact" element={<Contact />} />
+    //     <Route path="techacademy" element={<TechAcademy />} />
+    //     <Route
+    //       path="businesssupportacademy"
+    //       element={<BusinessSupportAcademy />}
+    //     />
+    //     <Route path="employability" element={<Employability />} />
+    //     <Route path="entrepreneurship" element={<Entrepreneurship />} />
+    //     <Route path="studentloan" element={<Studentloan />} />
+    //     <Route path="scholarships" element={<Scholarships />} />
+    //     <Route path="trainingbond" element={<Trainingbond />} />
+    //     <Route path="careers" element={<Careers />} />
+    //     {/* <Route path="vacancy" element={<Vacancy />} /> */}
+    //     <Route path="privacypolicy" element={<PrivacyPolicy />} />
+    //     <Route path="underconstruction" element={<UnderConstruction />} />
+    //     <Route path="*" element={<Error404 />} />
 
-        {/* course info pages */}
-        {/* techacademy */}
-        <Route path="frontend" element={<Frontend />} />
-        <Route path="backend" element={<Backend />} />
-        <Route path="fullstack" element={<Fullstack />} />
-        <Route path="devops" element={<Devops />} />
-        <Route path="architectdesign" element={<Architectdesign />} />
-        <Route path="architecttech" element={<Architecttech />} />
-        <Route path="relationaldb" element={<Relationaldb />} />
-        <Route path="riskmgt" element={<Riskmgt />} />
-        <Route path="reverseengineering" element={<Reverseengineering />} />
-        <Route path="pentesting" element={<Pentesting />} />
-        <Route path="bigdata" element={<Bigdata />} />
-        <Route path="python" element={<Python />} />
-        <Route path="iot" element={<IoT />} />
-        <Route path="securityaudit" element={<Securityaudit />} />
+    //     {/* course info pages */}
+    //     {/* techacademy */}
+    //     <Route path="frontend" element={<Frontend />} />
+    //     <Route path="backend" element={<Backend />} />
+    //     <Route path="fullstack" element={<Fullstack />} />
+    //     <Route path="devops" element={<Devops />} />
+    //     <Route path="architectdesign" element={<Architectdesign />} />
+    //     <Route path="architecttech" element={<Architecttech />} />
+    //     <Route path="relationaldb" element={<Relationaldb />} />
+    //     <Route path="riskmgt" element={<Riskmgt />} />
+    //     <Route path="reverseengineering" element={<Reverseengineering />} />
+    //     <Route path="pentesting" element={<Pentesting />} />
+    //     <Route path="bigdata" element={<Bigdata />} />
+    //     <Route path="python" element={<Python />} />
+    //     <Route path="iot" element={<IoT />} />
+    //     <Route path="securityaudit" element={<Securityaudit />} />
 
-        <Route path="sales" element={<Sales />} />
-        <Route path="customerexperience" element={<Customerexperience />} />
-        <Route path="digitalmarketing" element={<Digitalmarketing />} />
-        <Route path="videoediting" element={<Videoediting />} />
-        <Route path="stillandmotion" element={<Stillandmotion />} />
-        <Route path="businessupport" element={<BusinessSupportAcademy />} />
-        <Route path="digitaladmintools" element={<DigitalAdminTools />} />
-        {/* <Route path="business" element={<Business />} />
-				<Route path="contentmgt" element={<ContentMgt />} />
-				<Route path="criticalthinking" element={<Criticalthinking />} />
-				<Route path="devopportunity" element={<Devopportunity />} />
-				<Route path="financing" element={<Financing />} />
-				<Route path="growthstrategy" element={<Growth />} />
-				<Route path="personaldevt" element={<Personaldevt />} />
-				<Route path="successfulteam" element={<Successfulteam />} />
-				<Route path="workshop" element={<Workshop />} /> */}
-      </Routes>
+    //     <Route path="sales" element={<Sales />} />
+    //     <Route path="customerexperience" element={<Customerexperience />} />
+    //     <Route path="digitalmarketing" element={<Digitalmarketing />} />
+    //     <Route path="videoediting" element={<Videoediting />} />
+    //     <Route path="stillandmotion" element={<Stillandmotion />} />
+    //     <Route path="businessupport" element={<BusinessSupportAcademy />} />
+    //     <Route path="digitaladmintools" element={<DigitalAdminTools />} />
+    //     {/* <Route path="business" element={<Business />} />
+    // 		<Route path="contentmgt" element={<ContentMgt />} />
+    // 		<Route path="criticalthinking" element={<Criticalthinking />} />
+    // 		<Route path="devopportunity" element={<Devopportunity />} />
+    // 		<Route path="financing" element={<Financing />} />
+    // 		<Route path="growthstrategy" element={<Growth />} />
+    // 		<Route path="personaldevt" element={<Personaldevt />} />
+    // 		<Route path="successfulteam" element={<Successfulteam />} />
+    // 		<Route path="workshop" element={<Workshop />} /> */}
+    //   </Routes>
 
-      <footer>
-        <Footer />
-      </footer>
+    //   <footer>
+    //     <Footer />
+    //   </footer>
+    // </div>
+    <div>
+      <LeaderboardCard />
     </div>
   );
 }
--- src/components/AllCards/InnkeeperPhaseTwoCards/AllTasksCards.js
+++ src/components/AllCards/InnkeeperPhaseTwoCards/AllTasksCards.js
@@ -5,3 +5,109 @@
 // LeaderboardExplanationCard
 // SubmissionsCard
 // GradingCard
+
+// Images
+import Gold_medal from "../../../assets/images/Phase_Two/Leaderboard/Gold_medal.png";
+import Silver_medal from "../../../assets/images/Phase_Two/Leaderboard/Silver_medal.png";
+import Bronze_medal from "../../../assets/images/Phase_Two/Leaderboard/Bronze_medal.png";
+
+export const TasksCard = () => {
+  return (
+    <div className="Card-background">
+      {/* <h1>Frontend Development</h1> */}
+      <div className="card card-wrap">
+        <table>
+          <tr className="Card-heading">
+            <td className="first">TASK TITLE</td>
+            <td>TASK POINTS</td>
+            <td>SUBMISSION STATUS</td>
+            <td>DEADLINE</td>
+            <td className="last">...</td>
+          </tr>
+          <tr className="Card-details ">
+            <td className="first">Introduction to javascript.</td>
+            <td>10</td>
+            <td>Submitted</td>
+            <td>July 12</td>
+            <td className="last">View</td>
+          </tr>
+          <tr className="Card-details">
+            <td className="first">Introduction to javascript.</td>
+            <td>10</td>
+            <td>Submitted</td>
+            <td>July 12</td>
+            <td className="last">View</td>
+          </tr>
+          <tr className="Card-details">
+            <td className="first">Introduction to javascript.</td>
+            <td>10</td>
+            <td>Submitted</td>
+            <td>July 12</td>
+            <td className="last">View</td>
+          </tr>
+          <tr className="Card-details">
+            <td className="first">Introduction to javascript.</td>
+            <td>10</td>
+            <td>Submitted</td>
+            <td>July 12</td>
+            <td className="last">View</td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export const LeaderboardCard = () => {
+  return (
+    <div className="Card-background">
+      <div className="card card-wrap">
+        <table>
+          <tr className="Card-heading LeaderboardCard-heading">
+            <td className="first">POSITION</td>
+            <td>NAME</td>
+            <td className="last">POINTS</td>
+          </tr>
+          <tr className="Card-details ">
+            <td className="first leaderboard-serial">
+              <img
+                className="achievement-badge"
+                src={Gold_medal}
+                alt="Gold medal"
+              />
+            </td>
+            <td className="LeaderboardCard-studentName">Student 1</td>
+            <td className="last LeaderboardCard-pts">450</td>
+          </tr>
+          <tr className="Card-details">
+            <td className="first leaderboard-serial">
+              <img
+                className="achievement-badge"
+                src={Silver_medal}
+                alt="Silver medal"
+              />
+            </td>
+            <td className="LeaderboardCard-studentName">Student 2</td>
+            <td className="last LeaderboardCard-pts">450</td>
+          </tr>
+          <tr className="Card-details">
+            <td className="first leaderboard-serial">
+              <img
+                className="achievement-badge"
+                src={Bronze_medal}
+                alt="Bronze medal"
+              />
+            </td>
+            <td className="LeaderboardCard-studentName">Student 3</td>
+            <td className="last LeaderboardCard-pts">450</td>
+          </tr>
+          <tr className="Card-details">
+            <td className="first leaderboard-serial">4</td>
+            <td className="LeaderboardCard-studentName">Student 4</td>
+            <td className="last LeaderboardCard-pts">450</td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
+};
